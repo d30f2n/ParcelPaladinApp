21113ed14de096db630be829ea29147bb77fe22b
Ricky Huang <xd30f2nx@gmail.com>
2018-05-14 00:10:58 -0700

    I don't even know

--- app/src/main/java/com/parcelpaladin/d30f2n/parcelpaladinfirebase/LoginActivity.java
+++ app/src/main/java/com/parcelpaladin/d30f2n/parcelpaladinfirebase/LoginActivity.java
@@ -65,16 +65,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.user){
-            Toast.makeText(this, "User Activity", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
-            startActivity(intent);
-        }
         if(item.getItemId() == R.id.setting){
             Toast.makeText(this, "Setting Activity", Toast.LENGTH_SHORT).show();
-        }
-        if(item.getItemId() == R.id.login){
-            Toast.makeText(this, "Login Activity", Toast.LENGTH_SHORT).show();
         }
         if(item.getItemId() == R.id.logout){
             Toast.makeText(this, "Logout Activity", Toast.LENGTH_SHORT).show();
